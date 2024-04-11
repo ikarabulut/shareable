a72f7a2a6aac9cff7ff5244dab6231eb48da6cc7
Ismail Karabulut <i.karabulut93@gmail.com>
2024-04-11 13:00:43 -0500

    init

---
+++ src/main/java/com/ikarabulut/shareable/ShareableApplication.java
@@ -0,0 +1,13 @@
+package com.ikarabulut.shareable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareableApplication.class, args);
+	}
+
+}
